93c362c3eccbd972d30754e12fa0d561de31d600
sosunnyproject <parksosun1103@gmail.com>
2020-02-14 17:32:50 +0900

    express server example

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // package.json > "scripts" : node index.js 커맨드를 npm start 커맨드로 대체
 
-const express = require('express')
+const express = require('express');
 const app = express();
 const PORT = 4000;
 
@@ -8,6 +8,20 @@ function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+// app.post : request 비번, 데이터를 유저--> 서버로 보냈을 때, 댓글을 포스팅할 때 
+// app.get : response : 기본적인 브라우저가 페이지를 읽어올때, res.send('html 코드'), 
+// response: 요청을 받으면 응답을 해야함
+function handleHome(req, res){
+    console.log(req);
+    res.send("Hello from home");    // answer something
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile)
 app.listen(PORT, handleListening);
 
 // respond with hello world when GET request is made to the homepage
