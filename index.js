ce3c5c332449a1be6f610f52bff6960ebcf831e1
sosunnyproject <parksosun1103@gmail.com>
2020-02-14 16:26:40 +0900

    start express server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("HI");
+// package.json > "scripts" : node index.js 커맨드를 npm start 커맨드로 대체
+
+const express = require('express')
+const app = express();
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
+
+// respond with hello world when GET request is made to the homepage
+// app.get("/", function(req, res){
+//     res.send('hello world')
+// })
