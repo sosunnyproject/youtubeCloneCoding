fe0e7d6d7a2ba24979aafba3d80dea73f22e33aa
sosunnyproject <parksosun1103@gmail.com>
2020-03-11 10:16:58 +0900

    uploadVideo 3.6

--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,7 @@
 import routes from "./routes";
+import multer from "multer";
 
+const multerVideo = multer({ dest: "videos/" });
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
@@ -9,3 +11,5 @@ export const localsMiddleware = (req, res, next) => {
     };
     next();
 }
+
+export const uploadVideoMiddleware = multerVideo.single("videoFile");
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
 import { getUpload, postUpload, videoDetail, editVideo, deleteVideo, videos} from "../controllers/videoController";
+import {uploadVideoMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideoMiddleware, postUpload);
 
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
